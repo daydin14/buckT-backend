96d8aa5e0f001d8d7e232d42ff9c869ab1406446
David Aydin <david.aydin6@gmail.com>
2022-06-17 11:27:04 -0400

    Mongoose and Mongo Listeners

--- server.js
+++ server.js
@@ -10,3 +10,13 @@ const app = express();
 // Configure App Settings
 require("dotenv").config();
 const { PORT = 4000, MONGODB_URL } = process.env;
+
+// Connect to MongoDB
+mongoose.connect(MONGODB_URL);
+
+// Mongo Status Listeners
+mongoose.connection
+    .on("connected", () => console.log("Connected to MongoDB"))
+    .on("close", () => console.log("Disconnected from MongoDB"))
+    .on("error", (error) => console.log(`Error with MongoDB: ${error}`));
+
