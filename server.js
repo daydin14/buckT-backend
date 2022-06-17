0382f006d83f61ae3f28214f42dec4b3faf5f867
David Aydin <david.aydin6@gmail.com>
2022-06-17 11:23:17 -0400

    Initialized Express App

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
+
+// Initialize Express App
+const app = express();
